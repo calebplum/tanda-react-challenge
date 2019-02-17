99da6617f7eb631f9da4a57846baab87bbac5fa6
Caleb Plum <caleb.klein0@gmail.com>
2019-02-17 15:26:13 +1000

    Initial commit - Created React app basic structure

---
+++ tanda-react-challenge/src/components/LoginPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class LoginPage extends Component {
+    render() {
+        return (
+            <div className="Login-form">
+                This is a login form
+            </div>
+        );
+    }
+}
