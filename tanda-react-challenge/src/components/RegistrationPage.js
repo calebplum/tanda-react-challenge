30502fb2ff0c59a0f27a79dcb7b82f3983bcd3a6
Caleb Plum <caleb.klein0@gmail.com>
2019-02-17 17:13:31 +1000

    Added frontend proxy, added README to root project dir and implemented POSTing user details to the backend server on the registration page

--- tanda-react-challenge/src/components/RegistrationPage.js
+++ tanda-react-challenge/src/components/RegistrationPage.js
@@ -1,22 +1,71 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export class RegistrationPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            passwordConfirmation: ''
+        };
+
+        this.signUpUser = this.signUpUser.bind(this);
+    }
+
+    signUpUser() {
+        console.log(this.state);
+        axios({
+            method: 'post',
+            url: '/auth/signup',
+            data: this.state,
+            config: { headers: {'Content-Type': 'application/json'} }
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (response) {
+                console.log(response);
+            });
+    }
+
     render() {
         return (
             <div className="page-wrap">
                 <h1>Registration Page</h1>
                 <div className="registration-form">
-                    <form>
+
                         <label>
-                            Username:
+                            Name:
                         </label>
-                        <input type="text" id="username" name="username" />
+                        <input type="text" id="username" name="username" value={this.state.name}
+                            onChange={(event) =>
+                                this.setState({name: event.target.value})}/>
+                        <br />
                         <label>
-                            Password:
+                            Email:
                         </label>
-                        <input type="password" id="password" name="password" />
-                        <input type="submit" id="submit" value="submit" />
-                    </form>
+                        <input type="text" id="name" name="name" value={this.state.email}
+                               onChange={(event) =>
+                                   this.setState({email: event.target.value})}/>
+                        <br />
+                        <label>
+                            Password (6 characters minimum):
+                        </label>
+                        <input type="password" id="password" name="password" value={this.state.password}
+                               onChange={(event) =>
+                                   this.setState({password: event.target.value})}/>
+                        <br />
+                        <label>
+                            Confirm Password:
+                        </label>
+                        <input type="password" id="confirm-password" name="confirm-password" value={this.state.passwordConfirmation}
+                               onChange={(event) =>
+                                   this.setState({passwordConfirmation: event.target.value})}/>
+                        <br />
+                        <button id="submit" onClick={this.signUpUser}>Sign Up</button>
+
                 </div>
             </div>
         );
